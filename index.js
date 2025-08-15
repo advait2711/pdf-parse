b22f8704efe60430fdffed7834ae9d47645505fd
Advait <advaitsanoj@gmail.com>
2025-08-15 12:34:45 +0530

    fixed syntax1

--- index.js
+++ index.js
@@ -1,28 +1,5 @@
-const Fs = require('fs');
-const Pdf = require('./lib/pdf-parse.js');
+// index.js
+import pdf from './lib/pdf-parse.js';
+export default pdf; // export the function safely
 
-module.exports = Pdf;
-
-let isDebugMode = !module.parent; 
-
-//process.env.AUTO_KENT_DEBUG
-
-
-//for testing purpose
-
-//this part is causing a bug when importing the lib
-// if (isDebugMode) {
-
-//     let PDF_FILE = './test/data/05-versions-space.pdf';
-//     let dataBuffer = Fs.readFileSync(PDF_FILE);
-//     Pdf(dataBuffer).then(function(data) {
-//         Fs.writeFileSync(`${PDF_FILE}.txt`, data.text, {
-//             encoding: 'utf8',
-//             flag: 'w'
-//         });
-//         debugger;
-//     }).catch(function(err) {
-//         debugger;
-//     });
-
-
+// No demo/test code
